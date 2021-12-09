9154e0bf23bdc1adc2059c204bd56e31da54eeea
tomas sale <tomassale@hotmail.com>
2021-12-09 16:34:58 -0300

    Tarea 3

---
+++ app/src/components/NavBar.js
@@ -0,0 +1,19 @@
+const NavBar = () =>{
+    return(
+        <header>
+            <h1>Casona Wine</h1>
+            <ul>
+                <li>
+                    <a href="contacto.html">Contacto<span class="material-icons">call</span></a>
+                </li>
+                <li>
+                    <a href="carrito.html">Carrito<span class="material-icons">shopping_cart</span></a>
+                </li>
+                <li>
+                    <a href="productos.html">Productos<span class="material-icons">liquor</span></a>
+                </li>
+            </ul>
+        </header>
+    )
+}
+export default NavBar
